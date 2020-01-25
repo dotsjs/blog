9c6c1ccc55040e9245032c8b4a2dcc70d3d686c1
ParkEunwoo <dmsdn960@gmail.com>
2020-01-26 02:29:40 +0900

    setting more typescript

--- src/lib/createPages.ts
+++ src/lib/createPages.ts
@@ -1,10 +1,11 @@
 import { CreatePagesArgs } from 'gatsby';
 import path from 'path';
+import { Query } from '../graphql-types';
 
 export async function createPages({ actions, graphql }: CreatePagesArgs) {
     const { createPage } = actions;
 
-    const { data, errors } = await graphql(`
+    const { data, errors } = await graphql<Query>(`
         {
             allMarkdownRemark {
                 edges {
@@ -23,7 +24,7 @@ export async function createPages({ actions, graphql }: CreatePagesArgs) {
         throw errors;
     }
 
-    data.allMarkdownRemark.edges.forEach(({ node }: any) => {
+    data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
             path: String(node.frontmatter.title),
             context: {
