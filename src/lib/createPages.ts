fd7ef25cd02a9c5c0dff65b0ed8de2cffb7c2375
ParkEunwoo <dmsdn960@gmail.com>
2020-01-26 02:24:02 +0900

    transform markdown

--- src/lib/createPages.ts
+++ src/lib/createPages.ts
@@ -1,18 +1,35 @@
 import { CreatePagesArgs } from 'gatsby';
 import path from 'path';
 
-const pages = [
-    { id: 1, content: 'Gatsby 로 블로그 만들기' },
-    { id: 2, content: '거기에 타입스크립트 적용 해 보기' },
-    { id: 3, content: '확실히 어렵네요' },
-];
-
-export async function createPages({ actions }: CreatePagesArgs) {
+export async function createPages({ actions, graphql }: CreatePagesArgs) {
     const { createPage } = actions;
-    pages.forEach(page => {
+
+    const { data, errors } = await graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        html
+                        frontmatter {
+                            title
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    if (errors) {
+        throw errors;
+    }
+
+    data.allMarkdownRemark.edges.forEach(({ node }: any) => {
         createPage({
-            path: page.id.toString(),
-            context: page,
+            path: String(node.frontmatter.title),
+            context: {
+                html: node.html,
+                title: node.frontmatter.title,
+            },
             component: path.resolve(__dirname, '../templates/PostTemplate.tsx'),
         });
     });
